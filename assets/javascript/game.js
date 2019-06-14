bbd3c86595700999ccf87139dc8dfafd474548ca
gundrm <rachelm.gundy@gmail.com>
2019-06-13 17:26:34 -0700

    commit v2.0 update to simplify
    simplify game to psychic version - take out words as valid guesses reduce to letters

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,196 +1,98 @@
-var wins = 0;
-var losses = 0;
+// VARIABLES:
+var win = 0
+var losses = 0
+var maxGuesses = 10
+var guessAttempts = 0 
+var enteredGuesses = []
 
-var maxErrors = 9;
+var choices = ["a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z"]
 
-var wordDisplayLettersElement = document.getElementById("word-display-letters");
-var guessedLettersElement = document.getElementById("guessed-letters");
-var errorCountElement = document.getElementById("error-count");
-var winCountElement = document.getElementById("win-count");
-var lossCountElement = document.getElementById("loss-count");
 
-var blinkElements = document.getElementsByClassName("blinking");
-var alertLineElements = document.getElementsByClassName("alert-line");
+// function for computer to choose letter
+let compChoice = letterRand();
+console.log(compChoice)
+// console.log(index) // use to this console.log to confirm index
 
-var validGuesses = [ 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' ];
 
-var pressAnyKeyToStart = [
-	" ___                                       _                _              _               _   ",
-	"| _ \\ _ _  ___  ___ ___  __ _  _ _  _  _  | |__ ___  _  _  | |_  ___   ___| |_  __ _  _ _ | |_ ",
-	"|  _/| '_|/ -_)(_-<(_-< / _` || ' \\| || | | / // -_)| || | |  _|/ _ \\ (_-<|  _|/ _` || '_||  _|",
-	"|_|  |_|  \\___|/__//__/ \\__,_||_||_|\\_, | |_\\_\\\\___| \\_, |  \\__|\\___/ /__/ \\__|\\__,_||_|   \\__|",
-	"                                    |__/             |__/                                      "
-];
-var pressAnyKeyToReset = [
-	" ___                                       _                _                              _   ",
-	"| _ \\ _ _  ___  ___ ___  __ _  _ _  _  _  | |__ ___  _  _  | |_  ___    _ _  ___  ___ ___ | |_ ",
-	"|  _/| '_|/ -_)(_-<(_-< / _` || ' \\| || | | / // -_)| || | |  _|/ _ \\  | '_|/ -_)(_-</ -_)|  _|",
-	"|_|  |_|  \\___|/__//__/ \\__,_||_||_|\\_, | |_\\_\\\\___| \\_, |  \\__|\\___/  |_|  \\___|/__/\\___| \\__|",
-	"                                    |__/             |__/                                      "
-];
+//Elements from HTML
+let viewedGuesses = document.getElementById("guessedletters");
+const scoredWins = document.getElementById("winCount");
+const scoredLost = document.getElementById("lossCount");
+const wrongLetters = document.getElementById("errorCount");
 
-var youWin = [
-	"__  __ ____   __  __   _      __ ____ _  __",
-	"\\ \\/ // __ \\ / / / /  | | /| / //  _// |/ /",
-	" \\  // /_/ // /_/ /   | |/ |/ /_/ / /    / ",
-	" /_/ \\____/ \\____/    |__/|__//___//_/|_/  ",
-	"                                           "
-];
-var youLose = [
-	"__  __ ____   __  __  __   ____   ____ ____",
-	"\\ \\/ // __ \\ / / / / / /  / __ \\ / __// __/",
-	" \\  // /_/ // /_/ / / /__/ /_/ /_\\ \\ / _/  ",
-	" /_/ \\____/ \\____/ /____/\\____//___//___/  ",
-	"                                           "
-];
-var emptyAlert = [
-	"                                           ",
-	"                                           ",
-	"                                           ",
-	"                                           ",
-	"                                           "
-];
+scoredWins.textContent = win; // Will show user wins=0 at start
+scoredLost.textContent = losses; // Will show user lost=0 at start
+wrongLetters.textContent = guessAttempts; // Will show user guesses=0 at start
 
-var game = new Hangman();
-
-document.onkeyup = function(event) {
-	var userGuess = event.key;
-
-	if (!game.gameOver) {
-		if (validGuesses.includes(userGuess) && !game.guessedLetters.includes(userGuess)) {
-			game.checkGuess(userGuess);
-		}
-	} else {
-		game = new Hangman();
-		game.updatePageData();
-	}
+// function to randomize a letter
+function letterRand() {
+    // function for computer to choose letter
+    var index = [Math.floor(Math.random() * 26)];
+    return choices[index]
+    console.log(compChoice)
+    console.log(index) //confirm index
 }
 
-window.setInterval(function() {
-	if (blinkElements.length > 0) {
-		if (game.guessedLetters.length === 0 || game.gameOver) {
-			if (blinkElements[0].style.opacity === "1") {
-				for (var i = 0; i < blinkElements.length; i++) {
-					blinkElements[i].style.opacity = "0";
-				}
-			} else {
-				for (var i = 0; i < blinkElements.length; i++) {
-					blinkElements[i].style.opacity = "1";
-				}
-			}
-		} else {
-			for (var i = 0; i < blinkElements.length; i++) {
-				blinkElements[i].style.opacity = "0";
-			}
-		}
-	}
-}, 750);
+// Function CAPTURES KEY PRESSED BY USER
+// take key that's press - push it to entered guess array (use question game as example)
 
-function Hangman() {
-	this.wordList = [
-		"asphyxiation",
-		"justice",
-		"death",
-		"fading",
-		"punishment",
-		"sentence",
-		"execution",
-		"unjust",
-		"struggle",
-		"throat",
-		"trachea",
-		"breathing",
-		"oxygen",
-		"gasping",
-		"suffering",
-		"spectators",
-		"gallows",
-		"powerless"
-	]
+    //pseudo code for IF statments:
+        // Function MARKS LETTERS, WINS, LOST
+        // 1) if guessed amout is greater than allowed guesses
+        // 2) ELSE if no --> match = increase wins and start game over
+        // 3) ELSE if no --> no match = add letter and allow another guess
+        // 4) ELSE if yes --> no match = increase losses and start another game
+        // Next, we give JavaScript a function to execute when onkeyup event fires.
+document.onkeyup = function (event) {
+    enteredGuesses.push(event.key)
+    viewedGuesses.textContent = enteredGuesses.join(); 
+    
+    console.log('compChoice?', compChoice)
+    console.log(compChoice === event.key, compChoice, event.key)
 
-	this.word = this.wordList[Math.floor(Math.random() * this.wordList.length)];
-	this.guessedLetters = [];
-	this.errors = 0;
-	this.visibleLetters = [];
-	this.gameOver = false;
-	this.alertLines = emptyAlert;
-	for (var i = 0; i < this.word.length; i++) {
-		this.visibleLetters[i] = (false);
-	}
+    if (compChoice === event.key) { //if wins = guessed letter
+        console.log('winning?', win, compChoice)
+        win++; 
+        scoredWins.textContent = win; //increase wins on score board (when wins = guessed letter)
+        guessAttempts=0;
+        enteredGuesses = [];
+        compChoice = letterRand(); //after winning - generate new random letter (start new game)
+    } 
+    else if (guessAttempts == maxGuesses) { //if entered guess = 10
+        //compChoice !== event.key;
+        losses++;
+        scoredLost.textContent = losses; // increase lost on score board
+        guessAttempts=0; // reset guess attempts
+        enteredGuesses = []; // clear letters guessed
+    }
+    else {
+        guessAttempts++; 
+        wrongLetters.textContent = guessAttempts; // if previous conditions not met, increase guess attempts per guess
+    }
+
 }
-
-Hangman.prototype.checkGuess = function(char) {
-	this.guessedLetters.push(char);
-
-	var isInWord = false;
-	for (var i = 0; i < this.word.length; i++) {
-		if (this.word.charAt(i) === char) {
-			isInWord = true;
-			this.visibleLetters[i] = true;
-		}
-	}
-	if (!isInWord) {
-		this.errors++;
-	}
-
-	if (this.errors >= maxErrors) {
-		losses++;
-		this.alertLines = youLose;
-		this.gameOver = true;
-	}
-
-	if (!this.visibleLetters.includes(false)) {
-		wins++;
-		this.alertLines = youWin;
-		this.gameOver = true;
-	}
-
-	game.updatePageData();
-};
-
-Hangman.prototype.updatePageData = function() {
-	var tempString = "";
-	for (var i = 0; i < this.visibleLetters.length; i++) {
-		tempString += ((this.visibleLetters[i] || this.gameOver) ? this.word.charAt(i).toUpperCase() : "_");
-		if (i < (this.visibleLetters.length - 1)) tempString += " ";
-	}
-	wordDisplayLettersElement.textContent = tempString;
-
-	tempString = "";
-	for (var i = 0; i < this.guessedLetters.length; i++) {
-		tempString += (this.guessedLetters[i].toUpperCase());
-		if (i < (this.guessedLetters.length - 1)) tempString += " ";
-	}
-	for (var i = tempString.length; i < 51; i++) {
-		tempString += " ";
-	}
-	guessedLettersElement.textContent = tempString;
-
-	tempString = this.errors + " / " + maxErrors;
-	for (var i = tempString.length; i < 32; i++) {
-		tempString += " ";
-	}
-	errorCountElement.textContent = tempString;
-
-	tempString = wins + "";
-	for (var i = tempString.length; i < 45; i++) {
-		tempString += " ";
-	}
-	winCountElement.textContent = tempString;
-
-	tempString = losses + "";
-	for (var i = tempString.length; i < 43; i++) {
-		tempString += " ";
-	}
-	lossCountElement.textContent = tempString;
-
-	for (var i = 0; i < blinkElements.length; i++) {
-		blinkElements[i].textContent = (this.gameOver ? pressAnyKeyToReset[i] : pressAnyKeyToStart[i]);
-	}
-
-	for (var i = 0; i < alertLineElements.length; i++) {
-		alertLineElements[i].textContent = (this.alertLines[i]);
-	}
-}
-
-game.updatePageData();
